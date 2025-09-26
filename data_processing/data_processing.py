00acf2ee6a3ee9f0bf3c620b8177aa39bf860ee5
jendahejna <xhejna02@vutbr.cz>
2025-09-26 19:00:01 +0200

    feat: adding v_min/v_max to image name and color palette settings stored in json

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -19,7 +19,7 @@ first_run = True
 
 def collect_data_summary(df):
 
-    image_time = pd.to_datetime(df["Time"].iloc[0]).ceil('h')
+    image_time = pd.to_datetime(df["Time"].iloc[0]).ceil("h")
     image_hour = image_time.strftime("%Y-%m-%d_%H%M")
     image_name = f"{image_hour}.png"
 
@@ -27,16 +27,16 @@ def collect_data_summary(df):
 
 
 def prepare_data(
-        df: pd.DataFrame,
-        elevation_data: np.ndarray,
-        transform_matrix: Affine,
-        crs: CRS,
-        latitudes,
-        longitudes,
-        azimuths,
-        links,
-        technologies,
-        sides
+    df: pd.DataFrame,
+    elevation_data: np.ndarray,
+    transform_matrix: Affine,
+    crs: CRS,
+    latitudes,
+    longitudes,
+    azimuths,
+    links,
+    technologies,
+    sides,
 ) -> pd.DataFrame:
     df["Azimuth"] = azimuths
     df["Latitude"] = latitudes
@@ -49,8 +49,7 @@ def prepare_data(
 
     transformer = Transformer.from_crs("EPSG:4326", crs, always_xy=True)
     xs, ys = transformer.transform(
-        df["Longitude"].to_numpy(),
-        df["Latitude"].to_numpy()
+        df["Longitude"].to_numpy(), df["Latitude"].to_numpy()
     )
 
     inv_affine = ~transform_matrix
@@ -63,7 +62,9 @@ def prepare_data(
     in_bounds = (rows_i >= 0) & (rows_i < h) & (cols_i >= 0) & (cols_i < w)
 
     elevation = np.full(rows_i.shape, np.nan, dtype=np.float32)
-    elevation[in_bounds] = elevation_data[rows_i[in_bounds], cols_i[in_bounds]].astype(np.float32)
+    elevation[in_bounds] = elevation_data[rows_i[in_bounds], cols_i[in_bounds]].astype(
+        np.float32
+    )
     df["Elevation"] = elevation
 
     df["Hour"] = df["Time"].dt.hour.astype(np.int16)
@@ -71,7 +72,9 @@ def prepare_data(
     return df
 
 
-def process_data_round(config, db_ops, geo_proc, czech_rep, elevation_data, transform_matrix, crs):
+def process_data_round(
+    config, db_ops, geo_proc, czech_rep, elevation_data, transform_matrix, crs
+):
     global first_run
     start_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     backend_logger.info(f"Calculation started on {start_datetime}")
@@ -79,31 +82,52 @@ def process_data_round(config, db_ops, geo_proc, czech_rep, elevation_data, tran
     try:
 
         df = get_data(config)
-        latitudes, longitudes, azimuths, links, technologies, sides = db_ops.get_metadata(df)
-        df = prepare_data(df, elevation_data, transform_matrix, crs, latitudes, longitudes, azimuths, links,
-                          technologies,
-                          sides)
+        latitudes, longitudes, azimuths, links, technologies, sides = (
+            db_ops.get_metadata(df)
+        )
+        df = prepare_data(
+            df,
+            elevation_data,
+            transform_matrix,
+            crs,
+            latitudes,
+            longitudes,
+            azimuths,
+            links,
+            technologies,
+            sides,
+        )
         image_name, image_time = collect_data_summary(df)
 
         ml_cfg = config.get_ml()
-        df = temperature_predict(df, scaler_path=ml_cfg["scaler_path"], lstm_model_path=ml_cfg["lstm_path"])
+        df = temperature_predict(
+            df, scaler_path=ml_cfg["scaler_path"], lstm_model_path=ml_cfg["lstm_path"]
+        )
 
         itp = config.get_interpolation_config()
         grid = config.get_grid_config()
         grid_x, grid_y, grid_z = spatial_interpolation(
-            df, czech_rep, geo_proc, elevation_data, transform_matrix, crs,
+            df,
+            czech_rep,
+            geo_proc,
+            elevation_data,
+            transform_matrix,
+            crs,
             variogram_model=itp["variogram_model"],
             nlags=itp["nlags"],
             regression_model_type=itp["regression_model"],
             grid_x_points=grid["x_points"],
-            grid_y_points=grid["y_points"]
+            grid_y_points=grid["y_points"],
         )
 
         write_predictions(df, config)
-        color_scale_info= map_plotting(grid_x, grid_y, grid_z, czech_rep, image_name, config)
-        save_color_scale(color_scale_info,image_name, config)
+        color_scale_info = map_plotting(
+            grid_x, grid_y, grid_z, czech_rep, image_name, config
+        )
     except Exception as e:
-        backend_logger.error(f"Error during data processing round: {e}\n{traceback.format_exc()}")
+        backend_logger.error(
+            f"Error during data processing round: {e}\n{traceback.format_exc()}"
+        )
 
     finally:
         if "df" in locals():
---
+++ data_processing/saving_utils.py
@@ -0,0 +1,18 @@
+import json
+import os
+import logging
+
+backend_logger = logging.getLogger("backend_logger")
+
+
+def save_color_scale(color_scale_info, image_name, config):
+    paths = config.get_paths()
+    save_dir = paths["color_scale_dir"]
+    output_dir = save_dir
+    os.makedirs(output_dir, exist_ok=True)
+    color_scale_path = os.path.join(output_dir, f"{image_name}_color_scale.json")
+
+    with open(color_scale_path, "w") as f:
+        json.dump(color_scale_info, f, indent=4)
+
+    backend_logger.info(f"Color scale settings saved to {color_scale_path}")
